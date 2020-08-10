dbd2387acae7811a7bf21b1203b894848efb7345
kerusey <38439184+kerusey@users.noreply.github.com>
2020-08-10 17:59:44 +0300

    Update 1.2
    Added a bunch of new features:
     - Automatic convert .doc / .docs / .docx files to pdf. (CUPS does not support these formats)
     - Debugged & released interface between Telegram Helper bot and this project

--- PrinterServer.py
+++ PrinterServer.py
@@ -1,41 +1,49 @@
 #!/usr/bin/python3
 import cups
 import os
+import subprocess
 import imaplib
 import email
+from json import load
+from time import sleep
 from email.header import decode_header
-import json
-import shutil
-import time
+from shutil import rmtree
 from random import choice
 from string import ascii_lowercase
 
 baseDir = os.path.dirname(os.path.abspath(__file__)) + "/"
-EmailCredits = json.load(open(baseDir + "EmailCredits.json"))
+EmailCredits = load(open(baseDir + "EmailCredits.json"))
 ignoredExtensions = ["html", "css", "pyc"]
+allowedHosts = ["danila.lihh@gmail.com"]
 
 # Setting up email server
-casheFolder = "CasheEmailFolder"
+casheFolder = baseDir + "CasheEmailFolder"
 if not os.path.isdir(casheFolder):
 	os.mkdir(casheFolder)
 imap = imaplib.IMAP4_SSL("imap.gmail.com")
 imap.login(EmailCredits['login'], EmailCredits['password']) # loggining
 # Setting up email server
 
-print("Server has been configured!\nProcessing listening to your mail...")
-
 def genRandomString(length:int=15):
 	return ''.join(choice(ascii_lowercase) for i in range(length))
+
+def converter(path:str): # TODO remove primary file
+	parentPath = ''.join(name + '/' for name in path.split("/")[:-1])
+	subprocess.run(["libreoffice", "--headless", "--convert-to", "pdf", "--outdir", parentPath, path],
+				   stdout = subprocess.DEVNULL,
+				   stderr = subprocess.STDOUT)
+
+	return parentPath
 
 def printServer(fullFileNameToBePrinted:str, description:str = "maintance"): # Works pretty fine
 	connection = cups.Connection()
 	printerName = [item for item in connection.getPrinters()][0]
 
-	connection.printFile(printerName, fullFileNameToBePrinted, description, {})
+	connection.printFile(printerName, fullFileNameToBePrinted, description, {}) # TODO DOCX, DOC, DOCS converter to cups-supportable formats
 
 def getMessageObject(number:int):
 	messageObject = {}
-	# messagesNumber = int(imap.select("INBOX")[1][0])
+	messagesNumber = int(imap.select("INBOX")[1][0])
 	status, message = imap.fetch(str(number), "(RFC822)")
 	for resource in message:
 		if isinstance(resource, tuple):
@@ -43,14 +51,13 @@ def getMessageObject(number:int):
 			messageObject['topic'] = decode_header(message["Subject"])[0][0] # decoding header of the message
 			if isinstance(messageObject['topic'], bytes): # if sender is dummy and wrote a shit in the header
 				messageObject['topic'] = messageObject['topic'].decode()
-
 			senderInfo = message.get("From")
 			messageObject['senderName'] = senderInfo.split('<')[0][:-1]
 			messageObject['senderEmail'] = senderInfo.split('<')[1][:-1]
 			messageObject['destinationPath'] = casheFolder + "/" + genRandomString()
 			if message.is_multipart():
 				for part in message.walk():
-					# contentType = part.get_content_type()
+					contentType = part.get_content_type()
 					contentDisposition = str(part.get("Content-Disposition"))
 					try:
 						body = part.get_payload(decode=True).decode()
@@ -66,27 +73,33 @@ def getMessageObject(number:int):
 	return messageObject
 
 def main():
-	while(True):
-		currentNumberQueue = int(open(baseDir + "settings.txt").read())
-		messagesInMail = int(imap.select("INBOX")[1][0])
-
-		if (currentNumberQueue <= messagesInMail):
-			for message in range(currentNumberQueue, messagesInMail + 1):
-				currentMessageObject = getMessageObject(message)
+	currentNumberQueue = int(open(baseDir + "settings.txt").read())
+	messagesInMail = int(imap.select("INBOX")[1][0])
+	if (currentNumberQueue <= messagesInMail):
+		for message in range(currentNumberQueue, messagesInMail + 1):
+			currentMessageObject = getMessageObject(message)
+			if (currentMessageObject['senderEmail'] in allowedHosts):
 				if ('destinationPath' in currentMessageObject):
+					files = os.listdir(currentMessageObject['destinationPath'])
+					for index in range(len(files)):
+						files[index] = currentMessageObject['destinationPath'] + '/' + files[index]
+					for file in files:
+						for extension in ['docs', 'doc', 'docx']:
+							if (file.find(extension) != -1):
+								currentMessageObject['destinationPath'] = converter(file)
+								os.remove(file)
+								break
 					fileList = os.listdir(currentMessageObject['destinationPath'])
 					for item in fileList:
-						try:
-							if (not item.split(".")[1] in ignoredExtensions):
-								print(item)
-								printServer(currentMessageObject['destinationPath'] + "/" + item)
-						except Exception:
-							pass
-		else:
-			time.sleep(60)
+						if (not item in ignoredExtensions):
+							printServer(currentMessageObject['destinationPath'] + "/" + item)
+			else:
+				print("spam")
+	sleep(20)
 
-		shutil.rmtree(casheFolder)
-		os.mkdir(casheFolder)
-		open(baseDir + "settings.txt", "w").write(str(messagesInMail + 1))
+	rmtree(casheFolder)
+	os.mkdir(casheFolder)
+	open(baseDir + "settings.txt", "w").write(str(messagesInMail + 1))
 
-main()
+while(True):
+	main()
